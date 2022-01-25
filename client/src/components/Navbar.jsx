773b47a6fd6f3db03ff74d4d7d1581f861822ef5
david bassey <davidosky1@gmail.com>
2022-01-25 05:01:42 +0100

    Modify navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,8 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import Logo from '../../images/logo.png';
+
 const Navbar = () => {
   return (
     <h1>Navbar</h1>
